e1f8805fb98affb1cfdb862b8d17736ce45e0d24
Vitaliy <23neyron@gmail.com>
2016-02-22 08:41:18 +0300

    add fabric gp

--- example.fabfile.py
+++ example.fabfile.py
@@ -4,5 +4,5 @@ from fabric.api import run, env, cd, roles, lcd, local, sudo
 def gp():
 	lcd("/home/neyron/Projects/Kairos/")
 	local("sudo git add .")
-	local("sudo git commit -a")
+	local("sudo git commit")
 	local("sudo git push Kairos master")
